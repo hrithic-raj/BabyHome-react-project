bdb3f51d883ed7a5e84afd2225bf0efc2549b93a
hrj <105597967+hrj-730@users.noreply.github.com>
2024-09-07 04:27:16 +0530

    AuthContext -added

---
+++ src/contexts/AuthContext.jsx
@@ -0,0 +1,35 @@
+import React,{createContext, useState} from "react";
+
+export const AuthContext = createContext();
+
+export const AuthProvider = ({children})=>{
+    const [user,setUser]=useState();
+    const URL="http://localhost:5000/users";
+    const login= async(username,password)=>{
+        try{
+            const res = await axios.get(`${URL}?username=${username}&password=${password}`)
+            if(res.data.length>0){
+                const loggedInUser = res.data[0];
+                setUser(loggedInUser);
+                localStorage.setItem('userId',loggedInUser.id);
+                return loggedInUser;
+            }else{
+                throw new Error('invalid email or password');
+            }
+        }
+        catch(err){
+            console.error('Login error :',err);
+        }
+    };
+
+    const logout =()=>{
+        setUser(null);
+        localStorage.removeItem('userId');
+    };
+
+return(
+    <AuthContext.Provider value={{user,login,logout}}>
+        {children}
+    </AuthContext.Provider>
+);
+};
