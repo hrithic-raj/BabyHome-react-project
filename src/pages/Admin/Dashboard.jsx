655d87089ec8aba9bd1003a4fc82ddcafdb897df
hrj <105597967+hrj-730@users.noreply.github.com>
2024-09-17 22:18:26 +0530

    dashboard layout updated

--- src/pages/Admin/Dashboard.jsx
+++ src/pages/Admin/Dashboard.jsx
@@ -8,12 +8,15 @@ function Dashboard() {
         <AdminNavbar/>
         <div className='flex'>
         <Sidebar/>
-        <div className='mt-[100px] ms-20'>
-            <div className=''>Hi chelloooo</div>
-            <div className='w-[400px] h-[400px] border'></div>
-            <div className='w-[400px] h-[400px] border'></div>
-            <div className='w-[400px] h-[400px] border'></div>
-            <div className='w-[400px] h-[400px] border'></div>
+        <div className='mt-[120px] ms-20'>
+          <div className='grid grid-cols-4 gap-x-6 gap-y-6'>
+              <div className='w-[400px] h-[200px] border'></div>
+              <div className='w-[400px] h-[200px] border'></div>
+              <div className='w-[400px] h-[200px] border'></div>
+              <div className='w-[400px] h-[200px] border'></div>
+              <div className='w-[400px] h-[400px] border'></div>
+          </div>
+            
         </div>
         </div>
         
